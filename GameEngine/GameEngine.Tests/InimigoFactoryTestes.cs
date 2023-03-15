9a219228f761d8b50c662dfe4292a0f10be70d8c
danilo.silva <danilo.silva@msn.com>
2023-03-15 16:43:48 -0300

    Fazendo Asserções sobre Objetos

---
+++ GameEngine/GameEngine.Tests/InimigoFactoryTestes.cs
@@ -0,0 +1,61 @@
+﻿using GameEngine.Main;
+
+namespace GameEngine.Tests;
+
+public class InimigoFactoryTestes
+{
+    // Floating Object Types
+    [Fact]
+    public void CriaMonstroPorPadrao()
+    {
+        InimigoFactory sut = new InimigoFactory();
+
+        Inimigo inimigo = sut.Criar("Zombie");
+
+        Assert.IsType<Monstro>(inimigo);
+    }
+
+    [Fact]
+    public void VerificaTipoIncorretoMonstroPadrao()
+    {
+        InimigoFactory sut = new InimigoFactory();
+
+        Inimigo inimigo = sut.Criar("Zombie");
+
+        Assert.IsNotType<DateTime>(inimigo);
+    }
+
+    [Fact]
+    public void CriaChefe()
+    {
+        InimigoFactory sut = new InimigoFactory();
+
+        Inimigo inimigo = sut.Criar("Zombie King", true);
+
+        Assert.IsType<Chefe>(inimigo);
+    }
+
+    [Fact]
+    public void VerificaTipoRetornadoAposCriacaoChefe()
+    {
+        InimigoFactory sut = new InimigoFactory();
+
+        Inimigo inimigo = sut.Criar("Zombie King", true);
+
+        // Faz o assert e pega o retorno do Cast
+        Chefe chefe = Assert.IsType<Chefe>(inimigo);
+
+        // Assert adicional com o tipo do Objeto
+        Assert.Equal("Zombie King", chefe.Nome);
+    }
+
+    [Fact]
+    public void VerificaTipoDerivadoAposCriacaoChefe()
+    {
+        InimigoFactory sut = new InimigoFactory();
+
+        Inimigo inimigo = sut.Criar("Zombie King", true);
+
+        Assert.IsAssignableFrom<Inimigo>(inimigo);
+    }
+}
